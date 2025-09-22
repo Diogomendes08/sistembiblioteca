1e14cad923be15d840b7cea2bb116c170c0ab164
Diogomendes08 <159036667+Diogomendes08@users.noreply.github.com>
2025-09-22 08:06:37 -0300

    Update usuarios.py

--- usuarios.py
+++ usuarios.py
@@ -1,1 +1,24 @@
+# usuarios.py
+usuarios = []
 
+while True:
+    print("\n=== USUÁRIOS ===")
+    print("1. Add | 2. Ver | 0. Voltar")
+    op = input("Opção: ")
+    
+    if op == "1":
+        user = {
+            'id': int(input("ID: ")),
+            'nome': input("Nome: "),
+            'email': input("Email: "),
+            'telefone': input("Telefone: ")
+        }
+        usuarios.append(user)
+        print("✅ Added")
+    
+    elif op == "2":
+        for u in usuarios:
+            print(f"ID: {u['id']} | Nome: {u['nome']}")
+    
+    elif op == "0":
+        break
